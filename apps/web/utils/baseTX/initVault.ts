5a98a28064841d5319a2f51c7da0219a5eec9b37
zou <leftrightch@gmail.com>
2025-08-10 23:47:50 +0800

    feat: add tx

--- apps/web/utils/baseTX/initVault.ts
+++ apps/web/utils/baseTX/initVault.ts
@@ -2,10 +2,9 @@ import { Transaction } from "@mysten/sui/transactions";
 import { bcs } from "@mysten/sui/bcs";
 import { package_addr } from "../package";
 
-export const initVault = () => {
-    let tx = new Transaction();
+export const initVault = (tx: Transaction) => {
     const [SeaVault, OwnerCap] = tx.moveCall({
         target: `${package_addr}::seaVault::create_vault`
     });
-    return tx;
+    return [SeaVault, OwnerCap];
 };
---
+++ apps/web/utils/mailService/sendMail.ts
@@ -0,0 +1,17 @@
+  export const sendEmail = async (to, url) => {
+    try {
+      const response = await fetch("../api/mailService", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ to, url }),
+      });
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("發送請求失敗:", error);
+      throw error;
+    }
+}
