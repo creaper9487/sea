3a0fed8ab8ef7c1a2d5d331549cfa4ab336bf99b
creaper9487 <w0979563238@gmail.com>
2026-01-20 03:29:54 +0800

    edits

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   transpilePackages: ["@workspace/ui"],
   async rewrites() {
     return [
